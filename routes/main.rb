48f95c41fd3276020805d65201bf26ff391ac28a
Scott Galetto <scott.galetto@aon.com>
2019-09-18 10:58:10 -0500

    initial commit

--- routes/main.rb
+++ routes/main.rb
@@ -4,18 +4,7 @@ class MyApp < Sinatra::Application
     erb :main
   end
 
-  post '/form/process' do
-    @clean_mdn = params[:mdn]
-    if params[:mdn]
-      @body = "Thank you for sampling our new Sauza Sparkling Margarita and for Making It With A Lifeguard!  Click here to view the video: #{@url}"
-      MsgToolbox.alt_send_sms(params[:mdn], @body, 63485)
-      flash[:message] = 'A link has been sent to your mobile number.'
-    else
-      flash[:error] = 'Please enter your mobile number.'
-    end
-    redirect '/'
 
-  end
 
 
   get "/monitor" do
